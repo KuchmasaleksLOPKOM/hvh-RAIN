75fdc906c884eb65cabb79656e35395cc0de5826
MyLoveKitty-MLK <158452940+KuchmasaleksLOPKOM@users.noreply.github.com>
2025-09-22 16:46:14 +0700

    Delete dynamic-image-refresh.js

--- dynamic-image-refresh.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-  const img = document.getElementById('dynamic-image');
-  if (img) {
-    const src = img.src.split('?')[0];
-    img.src = src + '?t=' + new Date().getTime();
-  }
-}, 500);
